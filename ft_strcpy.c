26bf38af086b6c2d7e157b612c65601a1a6bf760
dummylanguages <dummylanguages@gmail.com>
2021-11-01 22:30:07 +0200

    fix: add libft.h header to ft_strcpy.c

--- ft_strcpy.c
+++ ft_strcpy.c
@@ -6,9 +6,11 @@
 /*   By: rodrodri <rodrodri@student.hive.fi >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/29 11:26:52 by rodrodri          #+#    #+#             */
-/*   Updated: 2021/11/01 11:56:34 by rodrodri         ###   ########.fr       */
+/*   Updated: 2021/11/01 13:19:40 by rodrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 char	*ft_strcpy(char *dest, const char *src)
 {
