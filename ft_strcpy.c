e8431cd2509e1949db20a11d2363d021669ceb20
dummylanguages <dummylanguages@gmail.com>
2021-11-02 18:42:52 +0200

    fix: refactor ft_strcpy.c (DRY!)

--- ft_strcpy.c
+++ ft_strcpy.c
@@ -6,22 +6,31 @@
 /*   By: rodrodri <rodrodri@student.hive.fi >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/29 11:26:52 by rodrodri          #+#    #+#             */
-/*   Updated: 2021/11/01 13:19:40 by rodrodri         ###   ########.fr       */
+/*   Updated: 2021/11/02 18:31:42 by rodrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strcpy(char *dest, const char *src)
+char	*ft_strcpy(char *dst, const char *src)
 {
-	int	i;
+	size_t	src_len;
 
-	i = 0;
-	while (src[i])
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = src[i];
-	return (dest);
+	src_len = ft_strlen(src);
+	ft_memcpy(dst, src, src_len);
+	ft_bzero(dst + src_len, 1);
+	return (dst);
 }
+
+/*
+**	int	i;
+**
+**	i = 0;
+**	while (src[i])
+**	{
+**		dst[i] = src[i];
+**		i++;
+**	}
+**	dst[i] = src[i];
+**	return (dst);
+*/
