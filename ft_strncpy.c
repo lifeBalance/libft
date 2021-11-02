3443c8b705a38883f77a4c2fc1e4b3793eb6a047
dummylanguages <dummylanguages@gmail.com>
2021-11-02 18:43:13 +0200

    fix: refactor ft_strncpy.c (DRY!)

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: rodrodri <rodrodri@student.hive.fi >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/29 11:26:52 by rodrodri          #+#    #+#             */
-/*   Updated: 2021/11/01 13:21:28 by rodrodri         ###   ########.fr       */
+/*   Updated: 2021/11/02 18:30:23 by rodrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,29 @@
 
 char	*ft_strncpy(char *dst, const char *src, size_t len)
 {
-	char	*ret;
+	size_t	src_len;
 
-	ret = dst;
-	while (*src && len)
+	src_len = ft_strlen(src);
+	if (src_len < len)
 	{
-		*dst++ = *src++;
-		len--;
+		ft_strcpy(dst, src);
+		ft_bzero(dst + src_len + 1, len - src_len);
 	}
-	while (len--)
-		*dst++ = '\0';
-	return (ret);
+	else
+		ft_memcpy(dst, src, len);
+	return (dst);
 }
+
+/*
+**	char	*ret;
+**
+**	ret = dst;
+**	while (*src && len)
+**	{
+**		*dst++ = *src++;
+**		len--;
+**	}
+**	while (len--)
+**		*dst++ = '\0';
+**	return (ret);
+*/
