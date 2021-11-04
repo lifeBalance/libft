aadf9f5bd434a6ac30272828a538b367b4a2feac
dummylanguages <dummylanguages@gmail.com>
2021-11-04 18:13:17 +0200

    refactor: ft_memmove.c: down to 6 lines using idiomatic code

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: rodrodri <rodrodri@student.hive.fi >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/30 14:41:03 by rodrodri          #+#    #+#             */
-/*   Updated: 2021/10/31 16:59:14 by rodrodri         ###   ########.fr       */
+/*   Updated: 2021/11/04 17:04:43 by rodrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,10 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	char	*destin;
-	char	*source;
-
-	destin = (char *)dst;
-	source = (char *)src;
-	if (source > destin)
-		ft_memcpy(destin, source, len);
+	if ((char *)src > (char *)dst)
+		ft_memcpy(dst, src, len);
 	else
 		while (len--)
-			destin[len] = source[len];
+			*(char *)(dst + len) = *(char *)(src + len);
 	return (dst);
 }
