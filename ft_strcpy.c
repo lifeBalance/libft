864e9d7157cf57cb1752a9dec4e57581e8ff1130
dummylanguages <dummylanguages@gmail.com>
2021-11-04 20:00:00 +0200

    refactor: ft_strcpy.c: down to 3 lines reusing code (DRY)

--- ft_strcpy.c
+++ ft_strcpy.c
@@ -6,7 +6,7 @@
 /*   By: rodrodri <rodrodri@student.hive.fi >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/29 11:26:52 by rodrodri          #+#    #+#             */
-/*   Updated: 2021/11/02 18:31:42 by rodrodri         ###   ########.fr       */
+/*   Updated: 2021/11/04 19:59:36 by rodrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,13 @@
 
 char	*ft_strcpy(char *dst, const char *src)
 {
-	size_t	src_len;
-
-	src_len = ft_strlen(src);
-	ft_memcpy(dst, src, src_len);
-	ft_bzero(dst + src_len, 1);
+	ft_memcpy(dst, src, ft_strlen(src));
+	ft_bzero(dst + ft_strlen(src), 1);
 	return (dst);
 }
 
 /*
+**  The old basecamp way, when we couln't reuse code...
 **	int	i;
 **
 **	i = 0;
