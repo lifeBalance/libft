cd71165a191259a220355dc07f5db716cdcd3c2f
dummylanguages <dummylanguages@gmail.com>
2021-11-04 16:59:47 +0200

    refactor: ft_memccpy.c: use casts and post-decrement in loop condition

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: rodrodri <rodrodri@student.hive.fi >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/29 11:26:52 by rodrodri          #+#    #+#             */
-/*   Updated: 2021/10/30 14:21:21 by rodrodri         ###   ########.fr       */
+/*   Updated: 2021/11/04 16:52:45 by rodrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,11 @@
 
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
-	unsigned char	*destin;
-	unsigned char	*source;
-	unsigned char	ch;
-	size_t			i;
-
-	destin = (unsigned char *)dst;
-	source = (unsigned char *)src;
-	ch = (unsigned char)c;
-	i = 0;
-	while (i < n)
+	while (n--)
 	{
-		destin[i] = source[i];
-		if (source[i] == c)
-			return (dst + i + 1);
-		i++;
+		*(unsigned char *)dst++ = *(unsigned char *)src++;
+		if (*(unsigned char *)(src - 1) == (unsigned char)c)
+			return (dst);
 	}
 	return (NULL);
 }
