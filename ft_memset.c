52cd7f4f11f599ec3a6697e2c314d94656bd4be0
dummylanguages <dummylanguages@gmail.com>
2021-11-04 15:45:45 +0200

    refactor: ft_memset.c: use post-decrement in loop test and post-increment in pointer a

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: rodrodri <rodrodri@student.hive.fi >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/29 13:35:32 by rodrodri          #+#    #+#             */
-/*   Updated: 2021/10/29 22:50:47 by rodrodri         ###   ########.fr       */
+/*   Updated: 2021/11/04 09:26:52 by rodrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,7 @@ void	*ft_memset(void *b, int c, size_t len)
 	unsigned char	*a;
 
 	a = (unsigned char *)b;
-	while (len)
-	{
-		*a = (unsigned char)c;
-		a++;
-		len--;
-	}
+	while (len--)
+		*a++ = (unsigned char)c;
 	return (b);
 }
