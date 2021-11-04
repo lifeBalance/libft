66ca567559977e47a057b55dfa396a12d241a11f
dummylanguages <dummylanguages@gmail.com>
2021-11-04 18:39:26 +0200

    refactor: ft_memcmp.c: down to 4 lines using idiomatic code

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: rodrodri <rodrodri@student.hive.fi >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/30 14:41:03 by rodrodri          #+#    #+#             */
-/*   Updated: 2021/10/31 22:09:38 by rodrodri         ###   ########.fr       */
+/*   Updated: 2021/11/04 18:29:23 by rodrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,8 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	const char	*str1;
-	const char	*str2;
-
-	str1 = (const char *)s1;
-	str2 = (const char *)s2;
-	if (ft_strequ(str1, str2))
+	if (ft_strequ((char *)s1, (char *)s2))
 		return (0);
 	else
-		return (ft_strncmp(str1, str2, n));
+		return (ft_strncmp((char *)s1, (char *)s2, n));
 }
