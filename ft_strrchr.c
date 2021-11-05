0b68b99edc84c14c1656436141d111322b17ea11
dummylanguages <dummylanguages@gmail.com>
2021-11-05 11:36:22 +0200

    refactor ft_strrchr.c: rewrote the function in a clearer way (only 3 lines more)

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: rodrodri <rodrodri@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 23:42:06 by rodrodri          #+#    #+#             */
-/*   Updated: 2021/11/04 01:01:30 by rodrodri         ###   ########.fr       */
+/*   Updated: 2021/11/05 11:35:55 by rodrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,26 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	size_t	last;
+	int	last;
 
-	last = ft_strlen(s) + 1;
-	while (last--)
+	last = ft_strlen(s);
+	while (last >= 0)
+	{
 		if (s[last] == (char)c)
-			return ((char *)s + last);
+			return ((char *)(s + last));
+		last--;
+	}
 	return (NULL);
 }
+/*
+** Check this smarty-pants way:
+**{
+**	size_t	last;
+**
+**	last = ft_strlen(s) + 1;
+**	while (last--)
+**		if (s[last] == (char)c)
+**			return ((char *)s + last);
+**	return (NULL);
+**}
+*/
