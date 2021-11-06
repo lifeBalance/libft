b6240b85fddf5cd5f1e9d60ba514d6cf3e047ae6
dummylanguages <dummylanguages@gmail.com>
2021-11-06 11:10:36 +0200

    refactor ft_strequ.c; DRY!!!

--- ft_strequ.c
+++ ft_strequ.c
@@ -6,7 +6,7 @@
 /*   By: rodrodri <rodrodri@student.hive.fi >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/30 14:41:03 by rodrodri          #+#    #+#             */
-/*   Updated: 2021/10/31 22:32:17 by rodrodri         ###   ########.fr       */
+/*   Updated: 2021/11/06 11:09:24 by rodrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,25 @@
 
 int	ft_strequ(char const *s1, char const *s2)
 {
-	int	i;
-
-	i = 0;
-	while (s1[i])
-	{
-		if (s1[i] != s2[i])
-			return (0);
-		i++;
-	}
-	if (s1[i] == s2[i])
+	if (ft_strcmp(s1, s2) == 0)
 		return (1);
-	else
-		return (0);
+	return (0);
 }
+
+/* You dissapoint me, kid...
+**{
+**	int	i;
+**
+**	i = 0;
+**	while (s1[i])
+**	{
+**		if (s1[i] != s2[i])
+**			return (0);
+**		i++;
+**	}
+**	if (s1[i] == s2[i])
+**		return (1);
+**	else
+**		return (0);
+**}
+*/
