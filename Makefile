7498f260e5a264c98de010b1a07ee0b305f1f6a7
dummylanguages <dummylanguages@gmail.com>
2021-11-06 13:32:51 +0200

    add ft_strnstr.c; edit Makefile and libft.h

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: rodrodri <rodrodri@student.hive.fi >       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/10/29 11:09:58 by rodrodri          #+#    #+#              #
-#    Updated: 2021/11/06 12:24:54 by rodrodri         ###   ########.fr        #
+#    Updated: 2021/11/06 13:25:28 by rodrodri         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -32,6 +32,7 @@ SRCS	=	ft_memset.c		\
 			ft_strchr.c		\
 			ft_strrchr.c	\
 			ft_strstr.c		\
+			ft_strnstr.c	\
 \
 			ft_strcmp.c		\
 			ft_strncmp.c	\
@@ -72,6 +73,7 @@ OBJS	=	ft_memset.o		\
 			ft_strchr.o		\
 			ft_strrchr.o	\
 			ft_strstr.o		\
+			ft_strnstr.o	\
 \
 			ft_strcmp.o		\
 			ft_strncmp.o	\
---
+++ ft_strnstr.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rodrodri <rodrodri@student.hive.fi >       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/30 14:41:03 by rodrodri          #+#    #+#             */
+/*   Updated: 2021/11/06 13:31:32 by rodrodri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	char	*substr;
+	char	*ret;
+
+	substr = ft_strsub(needle, 0, len);
+	ret = ft_strstr(haystack, substr);
+	ft_strdel(&substr);
+	return (ret);
+}
