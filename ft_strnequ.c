a7e4391b0a52f6e81d876eac3c19a3e9db3520f2
dummylanguages <dummylanguages@gmail.com>
2021-11-07 13:26:09 +0200

    refactor ft_strnequ.c; shorter, just 1 line

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: rodrodri <rodrodri@student.hive.fi >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/30 14:41:03 by rodrodri          #+#    #+#             */
-/*   Updated: 2021/11/06 10:58:21 by rodrodri         ###   ########.fr       */
+/*   Updated: 2021/11/07 13:24:07 by rodrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,5 @@
 
 int	ft_strnequ(char const *s1, char const *s2, size_t n)
 {
-	if (ft_strncmp(s1, s2, n) == 0)
-		return (1);
-	return (0);
+	return (ft_strncmp(s1, s2, n) == 0);
 }
