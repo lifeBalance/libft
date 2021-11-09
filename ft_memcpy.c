3d49d74c4d96bc64e83a8fb46acd62cbb9412d48
dummylanguages <dummylanguages@gmail.com>
2021-11-09 18:05:00 +0200

    fix ft_memcpy.c: avoid segfault when NULL args are received; return NULL instead

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: rodrodri <rodrodri@student.hive.fi >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/29 11:26:52 by rodrodri          #+#    #+#             */
-/*   Updated: 2021/11/02 15:29:09 by rodrodri         ###   ########.fr       */
+/*   Updated: 2021/11/09 17:49:01 by rodrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	char	*destin;
-	char	*source;
+	unsigned char	*pdst;
 
-	destin = (char *)dst;
-	source = (char *)src;
+	if (!dst || !src)
+		return (NULL);
+	pdst = (unsigned char *)dst;
 	while (n--)
-		*destin++ = *source++;
+		*pdst++ = *(unsigned char *)src++;
 	return (dst);
 }
