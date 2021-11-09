91e693bbd5a2455ebf80b87bebd69a312fadf0d4
dummylanguages <dummylanguages@gmail.com>
2021-11-09 17:04:59 +0200

    fix/refactor ft_bzero.c: rewrite it in terms of ft_memset and add NULL check

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: rodrodri <rodrodri@student.hive.fi >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/30 11:14:13 by rodrodri          #+#    #+#             */
-/*   Updated: 2021/11/04 09:02:00 by rodrodri         ###   ########.fr       */
+/*   Updated: 2021/11/09 16:59:15 by rodrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 void	ft_bzero(void *s, size_t n)
 {
-	while (n--)
-		*(char *)s++ = 0;
+	if (!s)
+		return ;
+	ft_memset(s, 0, n);
 }
