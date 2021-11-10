1ee069c44eb7ae3031050a12e72d54955a048d91
dummylanguages <dummylanguages@gmail.com>
2021-11-10 19:23:42 +0200

    fix ft_memmove.c: remove NULL checks (undefined behaviour according to C std)

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: rodrodri <rodrodri@student.hive.fi >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/30 14:41:03 by rodrodri          #+#    #+#             */
-/*   Updated: 2021/11/10 12:38:17 by rodrodri         ###   ########.fr       */
+/*   Updated: 2021/11/10 18:35:22 by rodrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	if (!src)
-		return (NULL);
 	if ((char *)src > (char *)dst)
 		ft_memcpy(dst, src, len);
 	else
