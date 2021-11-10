3d906b126f1fd0e235593fe68e08694c11920138
dummylanguages <dummylanguages@gmail.com>
2021-11-10 20:26:20 +0200

    retouch ft_memcmp.c: added const modifier to fn calls

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: rodrodri <rodrodri@student.hive.fi >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/30 14:41:03 by rodrodri          #+#    #+#             */
-/*   Updated: 2021/11/10 17:12:14 by rodrodri         ###   ########.fr       */
+/*   Updated: 2021/11/10 20:24:47 by rodrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	if (ft_strequ((char *)s1, (char *)s2))
+	if (ft_strequ((char const *)s1, (char const *)s2))
 		return (0);
 	else
-		return (ft_strncmp((char *)s1, (char *)s2, n));
+		return (ft_strncmp((const char *)s1, (const char *)s2, n));
 }
