3d340542d5823d16621aed95194db9b41af3d1ee
dummylanguages <dummylanguages@gmail.com>
2021-11-10 17:37:24 +0200

    fix ft_bzero.c: remove NULL checks (undefined behaviour according to C std)

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: rodrodri <rodrodri@student.hive.fi >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/30 11:14:13 by rodrodri          #+#    #+#             */
-/*   Updated: 2021/11/09 16:59:15 by rodrodri         ###   ########.fr       */
+/*   Updated: 2021/11/10 17:33:56 by rodrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,5 @@
 
 void	ft_bzero(void *s, size_t n)
 {
-	if (!s)
-		return ;
 	ft_memset(s, 0, n);
 }
