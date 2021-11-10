ef5dc1377bb7af659ce9c62fe2233e8599b0981d
dummylanguages <dummylanguages@gmail.com>
2021-11-10 15:43:24 +0200

    fix ft_memchr.c; added tests so it doesn't segfault when args are NULL

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: rodrodri <rodrodri@student.hive.fi >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/30 14:41:03 by rodrodri          #+#    #+#             */
-/*   Updated: 2021/11/04 18:02:27 by rodrodri         ###   ########.fr       */
+/*   Updated: 2021/11/10 15:35:35 by rodrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,10 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
+	if (!s)
+		return (NULL);
 	while (n--)
-	{
-		if (*(char *)s == (char)c)
-			return ((void *)s);
-		s++;
-	}
+		if (*(unsigned char *)s++ == (unsigned char)c)
+			return ((void *)s - 1);
 	return (NULL);
 }
