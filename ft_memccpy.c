50dc0ae618c6af700b6149acac69af71464cf932
dummylanguages <dummylanguages@gmail.com>
2021-11-10 18:34:40 +0200

    fix ft_memccpy.c: remove NULL checks (undefined behaviour according to C std)

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: rodrodri <rodrodri@student.hive.fi >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/29 11:26:52 by rodrodri          #+#    #+#             */
-/*   Updated: 2021/11/09 22:35:47 by rodrodri         ###   ########.fr       */
+/*   Updated: 2021/11/10 18:26:52 by rodrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
-	if (!dst || !src)
-		return (NULL);
 	while (n--)
 	{
 		*(unsigned char *)dst++ = *(unsigned char *)src++;
