1341ee142b6604d8595d5dd17eebbf853b5c3477
dummylanguages <dummylanguages@gmail.com>
2021-11-10 17:33:15 +0200

    fix ft_memset.c: remove NULL checks (undefined behaviour according to C std)

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: rodrodri <rodrodri@student.hive.fi >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/29 13:35:32 by rodrodri          #+#    #+#             */
-/*   Updated: 2021/11/09 16:11:22 by rodrodri         ###   ########.fr       */
+/*   Updated: 2021/11/10 17:22:44 by rodrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,8 @@ void	*ft_memset(void *b, int c, size_t len)
 {
 	unsigned char	*a;
 
-	if (b)
-	{
-		a = (unsigned char *)b;
-		while (len--)
-			*a++ = (unsigned char)c;
-		return (b);
-	}
-	return (NULL);
+	a = (unsigned char *)b;
+	while (len--)
+		*a++ = (unsigned char)c;
+	return (b);
 }
