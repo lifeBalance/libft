10cc0a7505a98a1cc0be08654b6f39cdab8e0fed
dummylanguages <dummylanguages@gmail.com>
2021-11-11 22:04:58 +0200

    fix ft_strlcat.c: add check for dstsize < 0 or dstlen > dstsize; also add tests

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: rodrodri <rodrodri@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 23:42:06 by rodrodri          #+#    #+#             */
-/*   Updated: 2021/11/04 23:05:44 by rodrodri         ###   ########.fr       */
+/*   Updated: 2021/11/11 21:34:17 by rodrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 
 	dstlen = ft_strlen(dst);
 	ft_memcpy(dst + dstlen, src, (dstsize - dstlen - 1));
-	ft_bzero((dst + dstlen + ft_strlen(src)), 1);
+	if (dstsize > 0 && dstlen < dstsize)
+		ft_bzero((dst + dstlen + ft_strlen(src)), 1);
 	return (dstlen + ft_strlen(src));
 }
