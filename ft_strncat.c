7ffaab818a56b224ac11dab5eeed9536893f57d6
dummylanguages <dummylanguages@gmail.com>
2021-11-11 18:22:16 +0200

    fix ft_strncat.c: the end result wasn't null-terminated in all cases

--- ft_strncat.c
+++ ft_strncat.c
@@ -6,7 +6,7 @@
 /*   By: rodrodri <rodrodri@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 23:42:06 by rodrodri          #+#    #+#             */
-/*   Updated: 2021/11/02 23:26:17 by rodrodri         ###   ########.fr       */
+/*   Updated: 2021/11/11 18:11:18 by rodrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,14 @@
 
 char	*ft_strncat(char *s1, const char *s2, size_t n)
 {
-	ft_strncpy(s1 + ft_strlen(s1), s2, n);
+	char	*start;
+	size_t	amount;
+
+	start = s1 + ft_strlen(s1);
+	amount = n;
+	if (n > ft_strlen(s2))
+		amount = ft_strlen(s2);
+	ft_memcpy(start, s2, amount);
+	ft_bzero(start + amount, 1);
 	return (s1);
 }
-
-/*
-**	char	*ret;
-**
-**	ret = s1;
-**	while (*s1)
-**		s1++;
-**	while (*s2 && n--)
-**		*s1++ = *s2++;
-**	*s1 = '\0';
-**	return (ret);
-*/
