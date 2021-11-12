0a6cef1c2f70d6a4eb5fc23295502efddc440618
Javier <javi@grey.local>
2021-11-12 21:08:40 +0200

    refactor ft_strcmp.c: remove an unnecessary test in the loop test; cleaned the tests

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: rodrodri <rodrodri@student.hive.fi >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/27 17:50:50 by rodrodri          #+#    #+#             */
-/*   Updated: 2021/10/29 16:22:39 by rodrodri         ###   ########.fr       */
+/*   Updated: 2021/11/12 20:39:18 by rodrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	ft_strcmp(const char *s1, const char *s2)
 {
-	while (*s1 && *s2 && (unsigned char)*s1 == (unsigned char)*s2)
+	while (*s1 && (unsigned char)*s1 == (unsigned char)*s2)
 	{
 		s1++;
 		s2++;
