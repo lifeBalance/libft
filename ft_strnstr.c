4a33e434bf65dc12f69267e0fe397bcd6bc6281f
dummylanguages <dummylanguages@gmail.com>
2021-11-12 13:46:12 +0200

    fix ft_strnstr.c: the len parameter refers to the haystack!

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: rodrodri <rodrodri@student.hive.fi >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/30 14:41:03 by rodrodri          #+#    #+#             */
-/*   Updated: 2021/11/06 13:31:32 by rodrodri         ###   ########.fr       */
+/*   Updated: 2021/11/12 13:41:08 by rodrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,15 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	char	*substr;
 	char	*ret;
+	char	*sub;
 
-	substr = ft_strsub(needle, 0, len);
-	ret = ft_strstr(haystack, substr);
-	ft_strdel(&substr);
-	return (ret);
+	substr = ft_strsub(haystack, 0, len);
+	sub = ft_strstr(substr, needle);
+	if (sub)
+	{
+		ret = ft_strstr(haystack, sub);
+		ft_strdel(&substr);
+		return (ret);
+	}
+	return (NULL);
 }
