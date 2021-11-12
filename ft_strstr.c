9dd55569894ac539e4cf290e3524fd109af730e2
dummylanguages <dummylanguages@gmail.com>
2021-11-12 11:41:51 +0200

    fix ft_strstr.c: adjust size of the substring used for comparison

--- ft_strstr.c
+++ ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: rodrodri <rodrodri@student.hive.fi >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/30 14:41:03 by rodrodri          #+#    #+#             */
-/*   Updated: 2021/11/06 12:31:48 by rodrodri         ###   ########.fr       */
+/*   Updated: 2021/11/12 11:30:16 by rodrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_strstr(const char *haystack, const char *needle)
 
 	if (ft_strequ(needle, ""))
 		return ((char *)haystack);
-	substr = ft_strnew(ft_strlen(needle) + 1);
+	substr = ft_strnew(ft_strlen(needle));
 	i = 0;
 	while (ft_strlen(haystack + i) >= ft_strlen(needle))
 	{
