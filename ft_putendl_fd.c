e5d3cc35f23739a2fbd6490de55d82c832a1e1b0
Javier <javi@grey.local>
2021-11-13 11:42:30 +0200

    refactor ft_putendl_fd.c: did not create new string in memory; down to TWO lines

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: rodrodri <rodrodri@student.hive.fi >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/27 00:38:52 by rodrodri          #+#    #+#             */
-/*   Updated: 2021/11/12 22:22:12 by rodrodri         ###   ########.fr       */
+/*   Updated: 2021/11/13 11:39:19 by rodrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,18 @@
 
 void	ft_putendl_fd(char const *s, int fd)
 {
-	char	*snl;
-
-	snl = ft_strnew(ft_strlen(s) + 1);
-	ft_strcpy(snl, s);
-	ft_strcat(snl, "\n");
-	ft_putstr_fd(snl, fd);
-	ft_strdel(&snl);
+	ft_putstr_fd(s, fd);
+	ft_putchar_fd('\n', fd);
 }
+/*
+** Check this smarty-pants way:
+**{
+**	char	*snl;
+**
+**	snl = ft_strnew(ft_strlen(s) + 1);
+**	ft_strcpy(snl, s);
+**	ft_strcat(snl, "\n");
+**	ft_putstr_fd(snl, fd);
+**	ft_strdel(&snl);
+**}
+*/
