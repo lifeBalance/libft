26f3bba7c329a8c44c18686a8aaed85bfe2e130a
Javier <javi@grey.local>
2021-11-13 13:31:51 +0200

    fix ft_strncmp.c: it was failing...

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: rodrodri <rodrodri@student.hive.fi >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/27 17:50:50 by rodrodri          #+#    #+#             */
-/*   Updated: 2021/10/29 16:34:44 by rodrodri         ###   ########.fr       */
+/*   Updated: 2021/11/13 13:29:34 by rodrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,15 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	while (*s1 && *s2 && (unsigned char)*s1 == (unsigned char)*s2 && n)
+	while (n >= 1 && *s1 && *s2)
 	{
+		if ((unsigned char)*s1 != (unsigned char)*s2)
+			break ;
 		s1++;
 		s2++;
 		n--;
 	}
+	if (!n)
+		return ((unsigned char)*(s1 - 1) - (unsigned char)*(s2 - 1));
 	return ((unsigned char)*s1 - (unsigned char)*s2);
 }
