5c4e99e02b7203b7c54bcb77a605d2f87bf9083c
Javier <javi@grey.local>
2021-11-13 17:11:13 +0200

    fix ft_memcmp.c: norm indentation issue

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: rodrodri <rodrodri@student.hive.fi >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/30 14:41:03 by rodrodri          #+#    #+#             */
-/*   Updated: 2021/11/12 21:27:18 by rodrodri         ###   ########.fr       */
+/*   Updated: 2021/11/13 17:10:28 by rodrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	while (n--)
-	if (*(unsigned char *)s1++ != *(unsigned char *)s2++)
-		return (*(unsigned char *)(s1 - 1) - *(unsigned char *)(s2 - 1));
+		if (*(unsigned char *)s1++ != *(unsigned char *)s2++)
+			return (*(unsigned char *)(s1 - 1) - *(unsigned char *)(s2 - 1));
 	return (0);
 }
