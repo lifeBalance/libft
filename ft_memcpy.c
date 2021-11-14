5aa35f233d0e16443298f7da3786589eb47f207c
Javier <javi@grey.local>
2021-11-14 21:28:43 +0200

    fix ft_memcpy.c: added protection against NULL args

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: rodrodri <rodrodri@student.hive.fi >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/29 11:26:52 by rodrodri          #+#    #+#             */
-/*   Updated: 2021/11/10 17:38:06 by rodrodri         ###   ########.fr       */
+/*   Updated: 2021/11/14 21:25:58 by rodrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	unsigned char	*pdst;
 
+	if (!dst || !src)
+		return (NULL);
 	pdst = (unsigned char *)dst;
 	while (n--)
 		*pdst++ = *(unsigned char *)src++;
