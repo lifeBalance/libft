fcb9e0e2de107286e83a2d74e3ecde8652c44fe9
Javier <javi@grey.local>
2021-11-14 22:23:42 +0200

    fix ft_strmapi.c: add NULL string argument protection

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: rodrodri <rodrodri@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/06 15:59:26 by rodrodri          #+#    #+#             */
-/*   Updated: 2021/11/14 20:07:40 by rodrodri         ###   ########.fr       */
+/*   Updated: 2021/11/14 22:22:45 by rodrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int i, char c))
 	char			*m;
 	unsigned int	j;
 
+	if (!s)
+		return (NULL);
 	m = ft_strnew(ft_strlen(s));
 	if (!m)
 		return (NULL);
