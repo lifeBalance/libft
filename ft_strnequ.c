738e8b6dc41b27f2a39d80d403e2f6151584b2e1
Javier <javi@grey.local>
2021-11-14 23:12:33 +0200

    fix ft_strenqu.c: add NULL string arguments protection

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: rodrodri <rodrodri@student.hive.fi >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/30 14:41:03 by rodrodri          #+#    #+#             */
-/*   Updated: 2021/11/13 13:32:19 by rodrodri         ###   ########.fr       */
+/*   Updated: 2021/11/14 23:11:27 by rodrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 
 int	ft_strnequ(char const *s1, char const *s2, size_t n)
 {
-	return (ft_strncmp(s1, s2, n) == 0);
+	if (s1 && s2)
+		return (ft_strncmp(s1, s2, n) == 0);
+	return (0);
 }
