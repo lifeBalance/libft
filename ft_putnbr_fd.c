4bb7c4f343f3fdb08d7fe356b71476b0984de048
Javier <javi@grey.local>
2021-11-14 20:03:24 +0200

    refactor ft_putnbr_fd.c: using ft_itoa wasn't allowed, bc it uses malloc

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: rodrodri <rodrodri@student.hive.fi >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/27 00:38:52 by rodrodri          #+#    #+#             */
-/*   Updated: 2021/11/09 11:26:25 by rodrodri         ###   ########.fr       */
+/*   Updated: 2021/11/14 19:58:45 by rodrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,37 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	*s;
+	int		positions;
 
-	s = ft_itoa(n);
-	if (s)
+	if (n == -2147483648)
 	{
-		ft_putstr_fd(s, fd);
-		ft_strdel(&s);
+		ft_putstr_fd("-2", fd);
+		n = 147483648;
+	}
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		n *= -1;
+	}
+	positions = 1;
+	while (n / positions >= 10)
+		positions *= 10;
+	while (positions >= 1)
+	{
+		ft_putchar_fd((n / positions) % 10 + '0', fd);
+		positions /= 10;
 	}
 }
+
+/* It would have been nice... (if malloc were allowed)
+**{
+**	char	*s;
+**
+**	s = ft_itoa(n);
+**	if (s)
+**	{
+**		ft_putstr_fd(s, fd);
+**		ft_strdel(&s);
+**	}
+**}
+*/
